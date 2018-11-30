4361a9e9c918bc4feff4d04e47dbe686b1d31a6c
pacien <pacien.trangirard@pacien.net>
2018-11-30 14:38:09 +0100

    add huffman encoder

---
+++ src/huffmanencoder.nim
@@ -0,0 +1,39 @@
+# gzip-like LZSS compressor
+# Copyright (C) 2018  Pacien TRAN-GIRARD
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import tables
+import integers, huffmantree, bitwriter
+
+type HuffmanEncoder*[T, U: SomeUnsignedInt] = object
+  codebook: TableRef[T, U]
+
+proc buildCodebook*[T, U](tree: HuffmanTreeNode[T], codeType: typedesc[U]): TableRef[T, U] =
+  var codebook = newTable[T, U]()
+  proc addCode(node: HuffmanTreeNode[T], path: U, depth: int) =
+    case node.kind:
+      of branch:
+        addCode(node.left, path, depth + 1)
+        addCode(node.right, path or (1.U shl depth), depth + 1)
+      of leaf:
+        codebook[node.value] = path
+  addCode(tree, 0.U, 0)
+  codebook
+
+proc encoder*[T, U](tree: HuffmanTreeNode[T], codeType: typedesc[U]): HuffmanEncoder[T, U] =
+  HuffmanEncoder[T, U](codebook: buildCodebook(tree, codeType))
+
+proc encode*[T, U](decoder: HuffmanEncoder[T, U], value: T): U =
+  decoder.codebook[value]
---
+++ tests/thuffmanencoder.nim
@@ -0,0 +1,39 @@
+# gzip-like LZSS compressor
+# Copyright (C) 2018  Pacien TRAN-GIRARD
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest, streams, tables
+import bitreader, bitwriter
+import huffmantree, huffmanencoder
+
+suite "huffencoder":
+  let tree = huffmanBranch(
+    huffmanLeaf(1'u),
+    huffmanBranch(
+      huffmanLeaf(2'u),
+      huffmanLeaf(3'u)))
+
+  test "buildCodebook":
+    let codebook = buildCodebook(tree, uint)
+    check codebook.len == 3
+    check codebook[1'u] == 0b0
+    check codebook[2'u] == 0b01
+    check codebook[3'u] == 0b11
+
+  test "encode":
+    let encoder = tree.encoder(uint)
+    check encoder.encode(1'u) == 0b0
+    check encoder.encode(2'u) == 0b01
+    check encoder.encode(3'u) == 0b11
